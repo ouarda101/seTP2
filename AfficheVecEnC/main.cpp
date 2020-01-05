9e17bc65aae1b32bdc646925d6c7c6ce1684db4b
ouarda101 <52488330+ouarda101@users.noreply.github.com>
2020-01-05 14:43:28 +0100

    Add files via upload

---
+++ AfficheVecEnC/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T []
+    return 0;
+}
+
+void AfficheVec (int * vector , int dimension )
+{
+
+    for (int i=0;i<dimension;i++)
+        {
+            cout<<*(vector+i);
+        }
+}
